c8a40d5bb1e482697fafbd5959559ad794eac79e
Shankar <nick_shanky@yahoo.co.in>
2020-09-21 01:03:02 -0700

    1.replace getline with readline 2.improve cleanup and identation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # myshell
 Building my own linux shell
+
+inspired from:
+https://www.cs.cornell.edu/courses/cs414/2004su/homework/shell/shell.html
+
+https://medium.com/@birnbera/if-you-build-simple-shell-they-will-come-855a28e2558b
--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 #include <sys/wait.h>
 #include <unistd.h> //required for fork()
 
-//#include <readline/readline.h>
-//#include <readline/history.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #define MAXIMUM_COMMAND_LENGTH 16 	//in english words
 #define DEFAULT_PATH_LEN 32 		//in bytes
@@ -20,30 +20,30 @@ int main(int argc, char *argv[], char *envp[])
 	printf("Welcome to my shell! \n");
 	printf("******************** \n");
 	
-	pid_t child;	    
-	char *command[16], *tok, *lineptr = NULL; // Not going to use commands with more than 15 words; extra room for NULL term
-	size_t i, n; 	// Number of bytes read into lineptr (mallocated internally by getline)
-	size_t pathLen = DEFAULT_PATH_LEN ; //setting initial path length to 32 characters
-	int status;
-	char *buf = (char*)malloc(sizeof(char)*pathLen);
+	pid_t 	child;	    
+	char 	*command[16], *tok, *lineptr = NULL; 	// Not going to use commands with more than 15 words; extra room for NULL term
+	size_t 	i, n; 					// Number of bytes read into lineptr (mallocated internally by getline)
+	size_t 	pathLen = DEFAULT_PATH_LEN ; 		//setting initial path length to 32 characters
+	int 	status;
+	char 	*cwdBuf = (char*)malloc(sizeof(char)*pathLen);
 
 	while(1)
 	{	
 		while(1)
 		{
-			if( NULL == getcwd(buf,pathLen)) //getcwd returned NULL, so check for error
+			if( NULL == getcwd(cwdBuf,pathLen)) //getcwd returned NULL, so check for error
 			{
 				if(ERANGE == errno) //initial path length not sufficient, we increase pathLen and reallocate buf
 				{
-					free(buf);
+					free(cwdBuf);
 					pathLen += PATH_LENGTH_INCREMENT;
-					buf = (char*) malloc(sizeof(char)*pathLen);
+					cwdBuf = (char*) malloc(sizeof(char)*pathLen);
 					printf("getcwd returned insufficient path\n");
 				}
 				else
 				{
 					printf("getcwd returned error: %s",strerror(errno));
-					free(buf);
+					free(cwdBuf);
 					break;
 				}
 			}
@@ -52,12 +52,19 @@ int main(int argc, char *argv[], char *envp[])
 			
 		} 
 
-		printf("myshell:%s$ ",buf);
-		
-	       	if (getline(&lineptr, &n, stdin) == -1)
+		printf("myshell:%s$ ",cwdBuf);
+	
+		if(NULL == (lineptr=readline("")))
 			break;
-
-	  	tok = strtok(lineptr, " \t\n\r"); // First call to strtok stores the line in a static variable; second arg is delimiters
+		
+		//strtok cannot parse empty char buffers
+		if(strlen(lineptr) > 0)
+			tok = strtok(lineptr, " \t\n\r"); // First call to strtok stores the line in a static variable; second arg is delimiters
+		else
+		{
+			free(lineptr); //lineptr must be freed before readline() can use it
+			continue;
+		}
 
 	  	for (i = 0; i < 16 && tok != NULL; i++) // strtok returns NULL once there are no more tokens
 
@@ -69,8 +76,20 @@ int main(int argc, char *argv[], char *envp[])
 		command[i] = NULL;
 
 		//checks for built-in commands which run without creating a process
-		if(0 == strcmp(command[0],"exit"))
+		
+		if(0 == strcmp(command[0],"exit")) //exits the shell
 			break;
+
+		if(0 == strcmp(command[0],"cd")) //process cd commands
+		{
+			if(0 != chdir(command[1]))
+			{
+				printf("error: (%s) \n", strerror(errno));
+			}
+
+			continue;
+		}
+
 
 		child = fork(); //fork creates a child process. returns 0 to parent process and child id to child process  on success else a error num
 
@@ -95,12 +114,19 @@ int main(int argc, char *argv[], char *envp[])
 			wait(&status);
 		}
 
+		if(NULL != lineptr)
+			free(lineptr);
+
+
 	}
 
 	putchar('\n');
-	free(lineptr);
-	if(buf!=NULL)
-		free(buf);
+
+	if(NULL != lineptr)	
+		free(lineptr);
+
+	if(NULL != cwdBuf)
+		free(cwdBuf);
 
 	printf("*****************************\n");
 	printf("Thank you for using my shell!\n");
